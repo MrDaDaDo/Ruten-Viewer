12e4c6cc3c9daa221589a15055bc5e72f55da66d
MrDaDaDo <42548841+MrDaDaDo@users.noreply.github.com>
2020-01-03 11:40:25 +0800

    Create 露天拍賣版面優化.user.js
    移除客尊屋超大banner

---
+++ 露天拍賣版面優化.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        露天拍賣版面優化
+// @author       MrDaDaDo
+// @include      /^https:\/\/class\.ruten\.com\.tw\/user\/(.*)\?s=pnhunyu(.*)/
+// @require      https://code.jquery.com/jquery-3.3.1.min.js
+// ==/UserScript==
+(function() {
+    var $ = window.jQuery;
+    $("#iframe_custstore_adv_html").remove();
+})();
