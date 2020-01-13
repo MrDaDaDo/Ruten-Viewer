539b96d51cd6219597a4bafaec8dcc42a1a775d4
MrDaDaDo <42548841+MrDaDaDo@users.noreply.github.com>
2020-01-13 13:44:17 +0800

    v1.0.1

--- Ruten-Viewer.user.js
+++ Ruten-Viewer.user.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @name        露天拍賣版面優化
+// @version      1.0.1
 // @author       MrDaDaDo
 // @include      /^https:\/\/class\.ruten\.com\.tw\/user\/(.*)\?s=(.*)/
 // @require      https://code.jquery.com/jquery-3.3.1.min.js
